606f966112e51a30899a1f447db6e78c81d02a10
Jonas Bögle <Jonas@sandstorm-projects.de>
2018-10-23 18:49:17 +0200

    readded install.sh

--- Dockerfile
+++ Dockerfile
@@ -4,30 +4,9 @@ LABEL maintainer="Max Schmitt <max@schmitt.mx>"
 LABEL description="Docker Image for the Teamspeak 3 and Discord MusicBot called SinusBot."
 
 RUN apt update \
-    && apt install -y --no-install-recommends ca-certificates bzip2 less curl x11vnc xvfb libxcursor1 libnss3 libegl1-mesa libasound2 libglib2.0-0 python libxcomposite-dev jq procps \
+    && apt install -y --no-install-recommends x11vnc xvfb libxcursor1 libnss3 libegl1-mesa libasound2 libglib2.0-0 libxcomposite-dev jq procps \
     && apt -q clean all \
     && rm -rf /tmp/* /var/tmp/*
 
-# Install TeamSpeak Client
-RUN VERSION=$(curl -s https://www.teamspeak.com/versions/client.json | jq -r '.linux.x86_64.version') \
-
-	# Download TeamSpeak client
-	&& echo "Downloading TeamSpeak Client v$VERSION..." \
-	&& curl -s -o TeamSpeak3-Client-linux_amd64.run "http://dl.4players.de/ts/releases/$VERSION/TeamSpeak3-Client-linux_amd64-$VERSION.run" \
-	&& echo "Downloaded TeamSpeak Client" \
-
-	# Install TeamSpeak Client
-	&& chmod +x TeamSpeak3-Client-linux_amd64.run \
-	&& yes | ./TeamSpeak3-Client-linux_amd64.run \
-	&& rm TeamSpeak3-Client-linux_amd64.run \
-
-	# Copy SinusBot plugin
-	&& mkdir TeamSpeak3-Client-linux_amd64/plugins \
-	&& cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins \
-
-	# Remove glx-integration lib
-	&& rm TeamSpeak3-Client-linux_amd64/xcbglintegrations/libqxcb-glx-integration.so \
-
-	# Set the TS3PATH to the config.ini
-	&& sed -i "s|^TS3Path.*|TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\"|g" config.ini \
-	&& echo "Successfully installed TeamSpeak Client"
+# Download/Install TeamSpeak Client
+RUN bash install.sh teamspeak
---
+++ discord/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+case "$1" in
+
+"sinusbot")
+	echo "Downloading SinusBot..."
+	curl -s https://www.sinusbot.com/pre/sinusbot-0.14.3-0e747fd.tar.bz2 | tar xj
+	chmod 755 sinusbot
+	mv scripts default_scripts
+	ln -s data/private.dat private.dat
+	cp config.ini.dist config.ini
+	sed -i "s|^TS3Path.*|TS3Path = \"\"|g" config.ini
+    echo "Successfully installed SinusBot"
+	;;
+"youtube-dl")
+	echo "Downloading youtube-dl..."
+	curl -s -o /usr/local/bin/youtube-dl https://yt-dl.org/downloads/latest/youtube-dl
+	chmod 755 /usr/local/bin/youtube-dl
+	echo 'YoutubeDLPath = "/usr/local/bin/youtube-dl"' >> config.ini
+	echo "Successfully installed youtube-dl"
+	;;
+"teamspeak")
+	echo "Installing TeamSpeak Client..."
+	# Get latest TeamSpeak client version
+	VERSION=$(wget -q -O - https://www.teamspeak.com/versions/client.json | jq -r '.linux.x86_64.version')
+
+	# Download TeamSpeak client
+	echo "Downloading TeamSpeak Client v$VERSION..."
+	curl -s -o TeamSpeak3-Client-linux_amd64.run "http://dl.4players.de/ts/releases/$VERSION/TeamSpeak3-Client-linux_amd64-$VERSION.run"
+
+	# Install TeamSpeak Client
+	chmod 755 TeamSpeak3-Client-linux_amd64.run
+	yes | ./TeamSpeak3-Client-linux_amd64.run
+	rm TeamSpeak3-Client-linux_amd64.run
+
+	# Copy SinusBot plugin
+	mkdir TeamSpeak3-Client-linux_amd64/plugins
+	cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins
+
+	# Remove glx-integration lib
+	rm TeamSpeak3-Client-linux_amd64/xcbglintegrations/libqxcb-glx-integration.so
+
+	# Set the TS3Path to the config.ini
+	sed -i "s|^TS3Path.*|TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\"|g" config.ini
+	echo "Successfully installed the TeamSpeak Client"
+	;;
+esac
